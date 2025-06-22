6dd6539e246b4e7fecfd4f24a5f5a56d4c438ace
Steven Jarosz <>
2025-06-22 07:49:34 -0400

    enhanced debugging

--- audiosdk/audiosdk/AudioRecorder.swift
+++ audiosdk/audiosdk/AudioRecorder.swift
@@ -2,6 +2,7 @@ import Foundation
 import AudioToolbox
 import AVFoundation
 import OSLog
+import Security
 
 public enum RecordingError: Error, LocalizedError {
     case general(String)
@@ -69,7 +70,7 @@ fileprivate final class ProcessTap {
         
         var tapID: AUAudioObjectID = .unknown
         var err = AudioHardwareCreateProcessTap(tapDescription, &tapID)
-        guard err == noErr else { throw RecordingError.general("Failed to create process tap: \(err)") }
+        guard err == noErr else { throw RecordingError.general("Failed to create process tap: \(osStatusDescription(err))") }
         self.processTapID = tapID
 
         let systemOutputID = try AudioObjectID.readDefaultSystemOutputDevice()
@@ -88,7 +89,7 @@ fileprivate final class ProcessTap {
         
         aggregateDeviceID = .unknown
         err = AudioHardwareCreateAggregateDevice(description as CFDictionary, &aggregateDeviceID)
-        guard err == noErr else { throw RecordingError.general("Failed to create aggregate device: \(err)") }
+        guard err == noErr else { throw RecordingError.general("Failed to create aggregate device: \(osStatusDescription(err))") }
 
         logger.debug("Aggregate device #\(self.aggregateDeviceID, privacy: .public) created.")
 
@@ -119,10 +120,10 @@ fileprivate final class ProcessTap {
                 self.logger.error("Failed to write audio buffer to file: \(error.localizedDescription)")
             }
         }
-        guard err == noErr else { throw RecordingError.general("Failed to create IO proc: \(err)") }
+        guard err == noErr else { throw RecordingError.general("Failed to create IO proc: \(osStatusDescription(err))") }
 
         err = AudioDeviceStart(aggregateDeviceID, deviceProcID)
-        guard err == noErr else { throw RecordingError.general("Failed to start audio device: \(err)") }
+        guard err == noErr else { throw RecordingError.general("Failed to start audio device: \(osStatusDescription(err))") }
         
         isRecording = true
         logger.debug("Recording started.")
@@ -175,7 +176,7 @@ fileprivate extension AudioObjectID {
         var address = AudioObjectPropertyAddress(mSelector: kAudioHardwarePropertyDefaultSystemOutputDevice, mScope: kAudioObjectPropertyScopeGlobal, mElement: kAudioObjectPropertyElementMain)
         var size = UInt32(MemoryLayout<AudioDeviceID>.size)
         let err = AudioObjectGetPropertyData(.system, &address, 0, nil, &size, &deviceID)
-        guard err == noErr else { throw RecordingError.general("Failed to get default system output device: \(err)") }
+        guard err == noErr else { throw RecordingError.general("Failed to get default system output device: \(osStatusDescription(err))") }
         return deviceID
     }
 
@@ -200,7 +201,7 @@ fileprivate extension AudioObjectID {
         )
 
         guard err == noErr else {
-            throw RecordingError.general("Failed to translate PID to object ID: \(err)")
+            throw RecordingError.general("Failed to translate PID to object ID: \(osStatusDescription(err))")
         }
 
         guard objectID != .unknown else {
@@ -217,7 +218,7 @@ fileprivate extension AudioObjectID {
         let err = withUnsafeMutablePointer(to: &uid) {
             AudioObjectGetPropertyData(self, &address, 0, nil, &size, $0)
         }
-        guard err == noErr else { throw RecordingError.general("Failed to read device UID for object \(self): \(err)") }
+        guard err == noErr else { throw RecordingError.general("Failed to read device UID for object \(self): \(osStatusDescription(err))") }
         return uid as String
     }
     
@@ -226,7 +227,7 @@ fileprivate extension AudioObjectID {
         var address = AudioObjectPropertyAddress(mSelector: kAudioTapPropertyFormat, mScope: kAudioObjectPropertyScopeGlobal, mElement: kAudioObjectPropertyElementMain)
         var size = UInt32(MemoryLayout<AudioStreamBasicDescription>.size)
         let err = AudioObjectGetPropertyData(self, &address, 0, nil, &size, &description)
-        guard err == noErr else { throw RecordingError.general("Failed to read audio tap stream description for object \(self): \(err)") }
+        guard err == noErr else { throw RecordingError.general("Failed to read audio tap stream description for object \(self): \(osStatusDescription(err))") }
         return description
     }
 
@@ -239,14 +240,14 @@ fileprivate extension AudioObjectID {
         var size: UInt32 = 0
         var err = AudioObjectGetPropertyDataSize(self, &address, 0, nil, &size)
 
-        guard err == noErr else { throw RecordingError.general("Failed to read size for property \(property) on object \(self): \(err)") }
+        guard err == noErr else { throw RecordingError.general("Failed to read size for property \(property) on object \(self): \(osStatusDescription(err))") }
 
         let pointer = UnsafeMutableRawPointer.allocate(byteCount: Int(size), alignment: MemoryLayout<T>.alignment)
         defer { pointer.deallocate() }
 
         err = AudioObjectGetPropertyData(self, &address, 0, nil, &size, pointer)
 
-        guard err == noErr else { throw RecordingError.general("Failed to read data for property \(property) on object \(self): \(err)") }
+        guard err == noErr else { throw RecordingError.general("Failed to read data for property \(property) on object \(self): \(osStatusDescription(err))") }
 
         if T.self == String.self {
             let cfString = pointer.load(as: CFString.self)
@@ -254,5 +255,30 @@ fileprivate extension AudioObjectID {
         }
 
         return pointer.load(as: T.self)
+    }
+}
+
+private func osStatusDescription(_ status: OSStatus) -> String {
+    if let errorMessage = SecCopyErrorMessageString(status, nil) {
+        return (errorMessage as String)
+    }
+
+    // Attempt to decode as a FourCC code
+    var code = status.bigEndian
+    let cString: [CChar] = [
+        CChar(truncatingIfNeeded: (code >> 24) & 0xFF),
+        CChar(truncatingIfNeeded: (code >> 16) & 0xFF),
+        CChar(truncatingIfNeeded: (code >> 8) & 0xFF),
+        CChar(truncatingIfNeeded: code & 0xFF),
+        0 // Null terminator
+    ]
+
+    // Use the standard C function `isprint` to check for printable characters.
+    let isPrintable = cString.dropLast().allSatisfy { isprint(Int32($0)) != 0 }
+
+    if isPrintable, let fourCC = String(cString: cString, encoding: .ascii)?.trimmingCharacters(in: .whitespaces), !fourCC.isEmpty {
+        return "'\(fourCC)' (\(status))"
+    } else {
+        return "Error code \(status)"
     }
 } 
